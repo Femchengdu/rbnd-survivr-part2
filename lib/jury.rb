d1d55b9f462aeb71997ecb4d757bf148a4e277e6
Femi Ajayi <femiajayi705@gmail.com>
2016-08-07 02:54:50 +0800

    Initial commit

---
+++ lib/jury.rb
@@ -0,0 +1,48 @@
+class Jury
+	def initialize 
+		@jury = []
+	end
+
+	def add_member j_member
+		@jury << j_member
+	end
+
+	def members
+		@jury
+	end
+
+	def members= j_members
+		@jury = j_members
+	end
+
+	def voting votes
+		votes_arr = []
+		members.each do
+			vot = votes[rand(2)]
+			votes_arr << vot
+			puts vot
+		end
+		return votes_arr
+	end
+
+	def cast_votes fin_array
+		votes_hash = Hash.new 0
+		votes_res = voting fin_array
+		votes_res.each do |voto|
+			votes_hash[voto] += 1
+		end
+		votes_hash
+	end
+
+	def report_votes results_hash
+		results_hash.each do |k,v|
+			puts "#{k}'s socore is #{v}"
+		end
+	end
+
+	def announce_winner results_hash
+		results_hash.select do |k, v|
+			return k if v > 3
+		end
+	end
+end
